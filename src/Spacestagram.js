bb3ad8ef6283a62a0a481bc5c5bc5968cad6a072
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-01-13 18:36:31 -0500

    packages

--- src/Spacestagram.js
+++ src/Spacestagram.js
@@ -1,32 +1,74 @@
 import './index.css';
 import React, { useState, useEffect } from 'react';
+import { month } from './constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faRocket, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const Spacestagram = () => {
 
 const [isLoading, setIsLoading] = useState(true);
-
-
-const fetchData = () => {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=rfOteRGGpmwtn0HxnwvPb8XyzId5aPaVChqaUczJ')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data) + setIsLoading(false)
-    })
-    .catch(error => console.error(error));
-}
+const [apiData, setApiData] = useState(null);
+const [buttonStatus, setButtonStatus] = useState(true);
 
 useEffect(() => {
-    fetchData
-})
+    const url = 'https://api.nasa.gov/planetary/apod?api_key=rfOteRGGpmwtn0HxnwvPb8XyzId5aPaVChqaUczJ&start_date=2016-08-06&end_date=2016-08-11';
 
+    const fetchData = async() => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setApiData(data)
+            setIsLoading(false);
+        }
+        catch(error){
+            console.log("error", error);
+        }
+    }
+    fetchData();
+}, []);
 
+const handleDateConversion = (date) => {
+    const copyDate = date;
+    const stringDate = new Date(date);
+    let outputDate = month[stringDate.getMonth()] + " " + copyDate.substring(8) + ", " + copyDate.substring(0, 4)
+    return outputDate;
+}
+
+const handleClick = () => {
+    buttonStatus ? setButtonStatus(false) : setButtonStatus(true)
+};
 
     if(!isLoading){
         return (
             <div>
-              <h1 className="title">Spacestagram</h1>
-                  <div>
-                  Pull API image here.
+                  <header className="title">
+                    <h1>Spacestagram <FontAwesomeIcon icon={faRocket} /></h1>
+                  </header>
+                  <div className="apiData-container">
+                      <ul>
+                        {apiData.map((item, index) => (
+                            <li className="item-container" key={index}>
+                            <div className="title-date-container">
+                            {item.title}
+                            <br/>
+                            {handleDateConversion(item.date)}
+                            <br/>
+                            </div>
+                            <img className="apiDataPicture" src={item.url} alt="NASA API PICTURE" />
+                            <br/>
+                            <div className="explanation-container">
+                            {item.explanation}
+                            </div>
+                            <br/>
+                            <div className="apiDataButton-container">
+                                {buttonStatus ?
+                                <button className="apiDataButton" onClick={handleClick}><FontAwesomeIcon icon={faThumbsUp} /></button> 
+                                : <button className="apiDataButtonLiked" onClick={handleClick}><FontAwesomeIcon icon={faHeart} /></button>
+                                }
+                            </div>
+                            </li>
+                        ))}
+                    </ul>
                   </div>
             </div>
         );
@@ -34,8 +76,13 @@ useEffect(() => {
 
     else  {
         return ( 
-            <div className="loading">
-                LOADING...
+            <div>
+                <header className="title">
+                    <h1>Spacestagram</h1>
+                </header>
+                <div className="loading">
+                    LOADING...
+                </div>
             </div>
             );
     }
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+export {month};
